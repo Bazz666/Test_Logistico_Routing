48aaf85d87f1d1ed5a39c4df135e362c9b6c24ff
bazz666 <herrera.se1992@gmail.com>
2022-11-27 16:37:36 -0300

    fix: omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable
+         :omniauthable, omniauth_providers: [:github]
 
   def self.from_omniauth(auth)  
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
