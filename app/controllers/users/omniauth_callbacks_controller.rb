8435b32f38dab756a407b888d2443ed52c7459b3
bazz666 <herrera.se1992@gmail.com>
2022-11-27 16:51:25 -0300

    fix:omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,13 @@
-class CallbacksController < Devise::OmniauthCallbacksController
-    
+# frozen_string_literal: true
 
-    def github
-        handle_auth "Github"
-    end
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+ 
+  
+  def github
+    handle_auth "Github"
+  end
 
-    def handle_auth(kind)
+  def handle_auth(kind)
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: kind
@@ -14,9 +16,13 @@ class CallbacksController < Devise::OmniauthCallbacksController
         session["devise.auth_data"] = request.env["omniauth.auth"].except(:extra)
         redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
-    end
-
-    def failure
-    redirect_to root_path, alert: "Failure. Please try again"
-    end
   end
+
+  def failure
+  redirect_to root_path, alert: "Failure. Please try again"
+  end
+  # The path used when OmniAuth fails
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :organizations
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  devise_for :users,controllers: {sessions: 'users/sessions' , omniauth_callbacks: 'users/omniauth_callbacks'}
   resources :routes
   resources :vehicles
   resources :drivers
